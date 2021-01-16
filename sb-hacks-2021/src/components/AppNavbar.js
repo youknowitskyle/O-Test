c99dab177e5316f74e2ee52c336b11e02c8f453d
Kyle Wong <kyle_wong@ucsb.edu>
2021-01-16 00:28:34 -0800

    added login logic to navbar

--- sb-hacks-2021/src/components/AppNavbar.js
+++ sb-hacks-2021/src/components/AppNavbar.js
@@ -1,31 +1,56 @@
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Nav from "react-bootstrap/Nav";
-import Button from "react-bootstrap/Button";
-import Image from "react-bootstrap/Image";
-import React from "react";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import React from 'react';
+import { FirebaseAuthConsumer } from '@react-firebase/auth';
+import firebase from 'firebase';
 
-function AppNavbar(props) {
-  const user = props.user;
-
-  return (
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">Diagnosis Test</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#features">Home</Nav.Link>
-      <Nav.Link href="#pricing">Contact</Nav.Link>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#memes">
-        Login
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-  );
+function AppNavbar() {
+	return (
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+			<Navbar.Brand href="#home">Diagnosis Test</Navbar.Brand>
+			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			<Navbar.Collapse id="responsive-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav.Link href="#features">Home</Nav.Link>
+					<Nav.Link href="#pricing">Contact</Nav.Link>
+				</Nav>
+				<Nav>
+					<FirebaseAuthConsumer>
+						{({ user, isSignedIn }) => {
+							{
+								if (isSignedIn === true) {
+									return (
+										<Nav.Link
+											onClick={() => {
+												firebase.app().auth().signOut();
+											}}
+										>
+											Logout
+										</Nav.Link>
+									);
+								} else {
+									return (
+										<Nav.Link
+											onClick={() => {
+												const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+												firebase.auth().signInWithPopup(googleAuthProvider);
+											}}
+										>
+											Login
+										</Nav.Link>
+									);
+								}
+							}
+						}}
+					</FirebaseAuthConsumer>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 }
 
 export default AppNavbar;
