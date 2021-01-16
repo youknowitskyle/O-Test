d6abaff20e81ec71a960474e9914941149f15216
Kyle <kyle_wong@ucsb.edu>
2021-01-16 15:13:03 -0800

    added basic flask integration

--- sb-hacks-2021/src/components/AppNavbar.js
+++ sb-hacks-2021/src/components/AppNavbar.js
@@ -1,65 +1,68 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
-import Image from 'react-bootstrap/Image';
-import React from 'react';
-import { FirebaseAuthConsumer } from '@react-firebase/auth';
-import firebase from 'firebase';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import React from "react";
+import { FirebaseAuthConsumer } from "@react-firebase/auth";
+import firebase from "firebase";
+import * as ROUTES from "../constants/routes";
 
 function AppNavbar(props) {
-	console.log(props);
-	return (
-		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-			<Navbar.Brand href="/">Diagnosis Test</Navbar.Brand>
-			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav className="mr-auto">
-					<Nav.Link href="/">Home</Nav.Link>
-					<Nav.Link href="/about-us">About Us</Nav.Link>
-					{props.isSignedIn && <Nav.Link href="/results">Results</Nav.Link>}
-				</Nav>
-				<Nav>
-					{props.isSignedIn === true ? (
-						<Nav.Link
-							onClick={() => {
-								firebase.app().auth().signOut();
-							}}
-						>
-							Logout
-						</Nav.Link>
-					) : (
-						<Nav.Link
-							onClick={async () => {
-								const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-								await firebase.auth().signInWithPopup(googleAuthProvider);
+  console.log(props);
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="/">Diagnosis Test</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href={ROUTES.LANDING}>Home</Nav.Link>
+          <Nav.Link href={ROUTES.ABOUT}>About</Nav.Link>
+          {props.isSignedIn && (
+            <Nav.Link href={ROUTES.RESULTS}>Results</Nav.Link>
+          )}
+        </Nav>
+        <Nav>
+          {props.isSignedIn === true ? (
+            <Nav.Link
+              onClick={() => {
+                firebase.app().auth().signOut();
+              }}
+            >
+              Logout
+            </Nav.Link>
+          ) : (
+            <Nav.Link
+              onClick={async () => {
+                const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+                await firebase.auth().signInWithPopup(googleAuthProvider);
 
-								const uid = firebase.auth().currentUser.uid;
-								const name = firebase.auth().currentUser.displayName;
-								const email = firebase.auth().currentUser.email;
-								const ref = firebase.database().ref('users');
+                const uid = firebase.auth().currentUser.uid;
+                const name = firebase.auth().currentUser.displayName;
+                const email = firebase.auth().currentUser.email;
+                const ref = firebase.database().ref("users");
 
-								ref.once('value', (snapshot) => {
-									if (!snapshot.hasChild(uid)) {
-										firebase
-											.database()
-											.ref('users/' + uid)
-											.set({
-												name: name,
-												email: email,
-											});
-									}
-								});
-							}}
-						>
-							Login
-						</Nav.Link>
-					)}
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
-	);
+                ref.once("value", (snapshot) => {
+                  if (!snapshot.hasChild(uid)) {
+                    firebase
+                      .database()
+                      .ref("users/" + uid)
+                      .set({
+                        name: name,
+                        email: email,
+                      });
+                  }
+                });
+              }}
+            >
+              Login
+            </Nav.Link>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default AppNavbar;
--- sb-hacks-2021/src/components/results.js
+++ sb-hacks-2021/src/components/results.js
@@ -1,36 +1,75 @@
-import React from 'react';
-import { useEffect } from 'react';
-import firebase from 'firebase';
-import { makeStyles, TextField, Button, Typography, Link } from '@material-ui/core';
+import React from "react";
+import { useEffect, useState } from "react";
+import firebase from "firebase";
+import {
+  makeStyles,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from "@material-ui/core";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
-	container: {
-		padding: 30,
-		alignItems: 'center',
-		justifyContent: 'center',
-		width: '100%',
-		textAlign: 'center',
-	},
+  container: {
+    padding: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    textAlign: "center",
+  },
 }));
 
 const Results = (props) => {
-	const classes = useStyles();
+  const classes = useStyles();
+  const [message, setMessage] = useState("");
+  const [results, setResults] = useState("");
 
-	useEffect(() => {
-		firebase.auth().onAuthStateChanged(function (user) {
-			if (user) {
-				// User is signed in; do nothing
-			} else {
-				props.history.push('/');
-			}
-		});
-	}, []);
+  useEffect(() => {
+    // check if user is signed in
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        // check if user has taken quiz
+        const ref = firebase.database().ref("users/" + user.uid);
 
-	return (
-		<div className={classes.container}>
-			<h1>Results: {props.user ? props.user.displayName : ''}</h1>
-		</div>
-	);
+        ref.on("value", (snapshot) => {
+          if (snapshot.val() && snapshot.val().results) {
+            setResults(snapshot.val().results);
+          }
+        });
+      } else {
+        props.history.push("/");
+      }
+    });
+
+    // testing backend
+    const path = "http://localhost:5000/ping";
+    axios
+      .get(path)
+      .then((res) => {
+        setMessage(res.data);
+      })
+      .catch((err) => {
+        // eslint-disable-next-line
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h3">
+        Results: {props.user ? props.user.displayName : ""}
+      </Typography>
+      <br />
+      {results ? (
+        <Typography variant="h4">Diagnosis: {results}</Typography>
+      ) : (
+        <Typography variant="h4">
+          Please take the quiz to receive your results
+        </Typography>
+      )}
+    </div>
+  );
 };
 
 export default Results;
