59bdb8e1adbb924b188a8726dc24d9c82045cf9a
Kaylee <kaylee@ucsb.edu>
2021-01-16 13:10:07 -0800

    init

--- sb-hacks-2021/src/components/home.js
+++ sb-hacks-2021/src/components/home.js
@@ -1,36 +1,53 @@
-import React from 'react';
-import AppNavbar from './AppNavbar';
-import { FirebaseAuthConsumer } from '@react-firebase/auth';
-import firebase from 'firebase';
-import { makeStyles, TextField, Button, Typography, Link } from '@material-ui/core';
+import React from "react";
+import AppNavbar from "./AppNavbar";
+import { FirebaseAuthConsumer } from "@react-firebase/auth";
+import firebase from "firebase";
+import {
+  makeStyles,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from "@material-ui/core";
+import AppJumbotron from "./AppJumbotron";
+
+import "./home.css";
 
 const useStyles = makeStyles((theme) => ({
-	container: {
-		padding: 30,
-		alignItems: 'center',
-		justifyContent: 'center',
-		width: '100%',
-		textAlign: 'center',
-	},
+  container: {
+    padding: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    textAlign: "center",
+  },
 }));
 
 const Home = () => {
-	const classes = useStyles();
-	return (
-		<div className={classes.container}>
-			<FirebaseAuthConsumer>
-				{({ user, isSignedIn }) => {
-					{
-						if (isSignedIn === true) {
-							return <div>Hello, {user.displayName}</div>;
-						} else {
-							return <div>Welcome! Please sign in.</div>;
-						}
-					}
-				}}
-			</FirebaseAuthConsumer>
+  const classes = useStyles();
+  return (
+    <div className="container-fluid">
+	<div className="row">
+		hello world
+	  </div>
+
+      <div className="row">
+        <div className="col-sm">
+
 		</div>
-	);
+        <div className="col-sm jumbo">
+			<AppJumbotron />
+		</div>
+        <div className="col-sm">
+
+		</div>
+      </div>
+
+	  <div className="row">
+		  hi
+	  </div>
+    </div>
+  );
 };
 
 export default Home;
